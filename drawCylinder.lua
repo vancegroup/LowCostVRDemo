787223a872b1106fcb1de259a414892f7d2fa867
Craig Disselkoen <cdissel@iastate.edu>
2013-06-23 16:52:14 -0500

    some extra comments. And fixed the order for the cursor transforms.

--- drawCylinder.lua
+++ drawCylinder.lua
@@ -3,11 +3,11 @@ button1 = gadget.DigitalInterface('VJButton1')
 
 cursor_geode = osg.Geode()
 cursor_geode:addDrawable(osg.ShapeDrawable(osg.Cone(Vecf(0,0,0), 0.1, 0.3)))   -- tip is 0.2 farther in the z direction
-cursor_xform = osg.MatrixTransform(device.matrix)
-cursor_xform:addChild(cursor_geode)
 cursor_permxform = Transform{ orientation = AngleAxis(Degrees(-90), Axis{1.0,0.0,0.0}) }
-cursor_permxform:addChild(cursor_xform)
-RelativeTo.World:addChild(cursor_permxform)
+cursor_permxform:addChild(cursor_geode)
+cursor_xform = osg.MatrixTransform(device.matrix)
+cursor_xform:addChild(cursor_permxform)
+RelativeTo.World:addChild(cursor_xform)
 
 Actions.addFrameAction(function()
     repeat
@@ -34,14 +34,14 @@ Actions.addFrameAction(function()
 	RelativeTo.World:addChild(permxform)
 	
     repeat
-		cursor_xform:setMatrix(device.matrix)
+		cursor_xform:setMatrix(device.matrix)  -- update cursor position
 		local endLoc = Vecf(device.position)
 		local centerPos = avgPosf(endLoc,startLoc)
 		local deltax = endLoc:x()-startLoc:x()
 		local deltay = endLoc:y()-startLoc:y()
 		local deltaz = endLoc:z()-startLoc:z()
 		cylinder:setCenter(centerPos)
-		local newradius = (deltax^2+deltaz^2)^0.5/2.0
+		local newradius = (deltax^2+deltaz^2)^0.5/2.0  -- the diameter is the xz-distance between startLoc and endLoc. Divide by 2 to get the radius. xz-distance is used because the cylinder expands in the xz-plane and cannot be expanded in y during this step (making the base).
 		if (newradius > 0.1) then
 			cylinder:setRadius( newradius )
 			print("updated radius")
