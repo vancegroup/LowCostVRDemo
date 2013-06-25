7201683a2566ed723b0bb28a5d596e6a51f6b57b
Lanya Butler <llbutler@colby.edu>
2013-06-25 15:44:54 -0500

    Testing two models

--- help.lua
+++ help.lua
@@ -1,34 +1,43 @@
 require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
 device = gadget.PositionInterface("VJWand")
+libraryButton = gadget.DigitalInterface("VJButton1")
+
 model_1 = Model("OSG/wiimote.ive")
-display = osg.DisplaySettings()
-print([[screen width]], display:getScreenWidth())
-print([[height]], display:getScreenHeight())
-print([[distance]], display:getScreenDistance())
+model_2 = Model("OSG/wiimote.ive")
+
+--[[display = osg.DisplaySettings()
+print("screen width", display:getScreenWidth())
+print("height", display:getScreenHeight())
+print("distance", display:getScreenDistance())
+]]
 
 xform1 = osg.AutoTransform()
 xform1:setPosition(Vec(-3.1, 2.85, 0.0))
 xform1:setAutoRotateMode(1)
 xform1:setAutoScaleToScreen(0)
 xform1:addChild(model_1)
-RelativeTo.Room:addChild(xform1)
 
---[[
-xform1 = Transform{
-	position = {-1,2,0},
-	orientation = AngleAxis(Degrees(0), Axis{0.0, 1.0, 0.0})}
-RelativeTo.Room:addChild(xform1)
-xform1:addChild(model_1)
-]]
---[[xform1 = Transform{
-	position = {0,1.3,0},
-	orientation = AngleAxis(Degrees(7), Axis{0.0, 1.0, 0.0})}
-xform2 = Transform{
-	orientation = AngleAxis(Degrees(-5), Axis{1.0, 0.0, 0.0})}
-RelativeTo.Room:addChild(xform2)
+xform2 = osg.AutoTransform()
+xform2:setPosition(Vec(-1.8, 2.85, 0.0))
+xform2:setAutoRotateMode(1)
+xform2:setAutoScaleToScreen(0)
+xform2:addChild(model_2)
 
-xform2:addChild(xform1)
-xform1:addChild(model_1)]]
+Actions.addFrameAction(function()
+	while true do
+		repeat
+			Actions.waitForRedraw()
+		until libraryButton.justPressed
+		
+		RelativeTo.Room:addChild(xform1)
+		RelativeTo.Room:addChild(xform2)
+		repeat
+			Actions.waitForRedraw()
+		until libraryButton.justPressed
+			RelativeTo.Room:removeChild(xform1)
+			RelativeTo.Room:removeChild(xform2)
+	end
+end)
 
 -- [[ How are you going to adjust for different screen sizes??]]
